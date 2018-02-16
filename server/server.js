531c445792883e73c0f93857c65b1829b9bd0b8d
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-16 21:02:59 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,12 +12,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/todos', (req, res) => {
     Todo.create(req.body)
         .then((todo) => {
-            res.json(todo);
+            res.send(todo);
         })
         .catch((err) => {
             res.status(400).send(err);
         });
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find()
+        .then((todos) => {
+            res.send({ todos });
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
