df6f1be8585815f345fb059cca19872af03f3c84
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-17 12:29:55 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
         .catch((err) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({ message: 'Deleted' });
+        })
+        .catch((err) => res.status(400).send());
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
 });
