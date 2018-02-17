e58a81fc051a24a73c7f6ba94b9ea541dfa9e316
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-17 11:40:17 +0200

    Config heroku for mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 //mongoose.set('debug', true);
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-app');
 
 module.exports.mongoose = mongoose;
