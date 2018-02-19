25fd86a8d88bffb1b9889b3dcad555106df8b55d
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-19 18:02:29 +0200

    Add POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bycrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     email: {
@@ -64,10 +64,24 @@ userSchema.statics.findByToken = function(token) {
     });
 };
 
+userSchema.statics.findByCredentials = function(email, password) {
+    return this.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) resolve(user);
+                else reject();
+            });
+        });
+    });
+};
+
 userSchema.pre('save', function(next) {
     if (this.isModified('password')) {
-        bycrypt.genSalt(10, (err, salt) => {
-            bycrypt.hash(this.password, salt, (err, hash) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(this.password, salt, (err, hash) => {
                 this.password = hash;
                 next();
             });
--- server/server.js
+++ server/server.js
@@ -98,7 +98,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-})
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            user
+                .generateAuthToken()
+                .then((token) => res.header('x-auth', token).send(user))
+        })
+        .catch((err) => {
+            res.status(400).send();
+        });
+});
 
 
 app.listen(process.env.PORT, () => {
